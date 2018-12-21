1d69f21d96556ebe8f733d076d3ddaa2e865df00
Christian Ivicevic <christian.ivicevic@comsysto.com>
2018-12-21 11:12:05 +0100

    :sparkles: Add naive caching of responses

--- packages/server/src/utils/PokeApiHelper.ts
+++ packages/server/src/utils/PokeApiHelper.ts
@@ -2,20 +2,29 @@ import axios from "axios";
 
 const apiEndpoint = "https://pokeapi.co/api/v2/";
 
-function buildUrl(endpoint: string) {
-    return `${apiEndpoint}${endpoint}`;
-}
+type PokeApiEndpoint = "region" | "language";
 
 export interface PokeApiWhere {
     id?: number;
     name?: string;
 }
 
-type PokeApiEndpoint = "region" | "language";
+// tslint:disable-next-line:no-any
+const cachedResponses: Record<string, any> = {};
+
+function buildUrl(endpoint: string) {
+    return `${apiEndpoint}${endpoint}`;
+}
 
 export async function fetchPokeApi<ResponseType>(endpoint: PokeApiEndpoint, where?: PokeApiWhere) {
     const apiUrl = buildUrl(`${endpoint}/${where !== undefined ? where.id || where.name : ""}`);
-    console.log(`GET ${apiUrl}`);
-    const request = await axios.get<ResponseType>(apiUrl);
-    return request.data;
+    if (cachedResponses[apiUrl] === undefined) {
+        console.log(`[From Server] GET ${apiUrl}`);
+        const { data } = await axios.get<ResponseType>(apiUrl);
+        cachedResponses[apiUrl] = data;
+        return data;
+    } else {
+        console.log(`[From Cache] GET ${apiUrl}`);
+        return cachedResponses[apiUrl] as ResponseType;
+    }
 }
