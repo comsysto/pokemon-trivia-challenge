f846d010edbcc3769134f1cfd1b2b523be50fdc4
Christian Ivicevic <christian.ivicevic@comsysto.com>
2018-12-30 14:45:24 +0100

    :white_check_mark: Add test skeletons

---
+++ packages/client/src/routes/explore/test/RegionPanelContainer.test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+describe("<RegionPanelContainer>", () => {
+    it("needs tests");
+});
---
+++ packages/client/src/routes/explore/test/ZonePanelContainer.test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+describe("<ZonePanelContainer>", () => {
+    it("needs tests");
+});
