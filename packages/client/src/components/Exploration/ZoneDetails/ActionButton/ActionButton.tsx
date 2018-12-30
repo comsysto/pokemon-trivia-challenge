631310d63fb08720fcf43188d98d92cee8cd5297
Christian Ivicevic <christian.ivicevic@comsysto.com>
2018-12-30 14:46:01 +0100

    :fire: Delete old mockup for zone details and quiz

--- packages/client/src/components/Exploration/ZoneDetails/ActionButton/ActionButton.tsx
+++
@@ -1,6 +0,0 @@
-import { Button, Intent } from "@blueprintjs/core";
-import React from "react";
-
-export function ActionButton() {
-    return <Button intent={Intent.PRIMARY} text="Start Exploration" large />;
-}
--- packages/client/src/components/Quiz/Encounter/Card/Card.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Card() {
-    const id = Math.floor(Math.random() * 650) + 0;
-
-    return <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />;
-}
